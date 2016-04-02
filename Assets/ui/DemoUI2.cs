c2442d13f95864795da003004ba8ec1fec6f6e0f
IronPeak <HrafnOrri1207@gmail.com>
2016-04-02 18:59:59 +0000

    3 Earths scenes

---
+++ Assets/ui/DemoUI2.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DemoUI2 : MonoBehaviour
+{
+
+    public Slider Earth1Mass;
+    public Slider Earth2Mass;
+    public Slider Earth3Mass;
+
+    public GravityObject Earth1;
+    public GravityObject Earth2;
+    public GravityObject Earth3;
+
+    void Update()
+    {
+        Earth1.Mass = Earth1Mass.value * 1000 - 500;
+        Earth2.Mass = Earth2Mass.value * 1000 - 500;
+        Earth3.Mass = Earth3Mass.value * 1000 - 500;
+    }
+
+}
