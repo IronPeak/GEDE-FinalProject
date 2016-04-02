205cfbd718ddbc7dbbc64040acc2ee86c68c1a71
Drulludanni <gudni14@ru.is>
2016-04-02 00:23:05 +0000

    vital fix to clipping

---
+++ Assets/FPSExamplestuff/Gun.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+    public GameObject projectile;
+    public float velocity;
+    public Vector3 gravity;
+    public float bouncyness = 1;
+    public float friction = 0;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1")) {
+            GameObject proj = (GameObject)Instantiate(projectile, transform.position + transform.forward*0.3f, Quaternion.identity);
+            PhysicsObject script = proj.GetComponent<PhysicsObject>();
+            script.SetGlobalGravity(gravity);
+            script.SetVelocity(transform.forward*velocity);
+            script.bouncyness = bouncyness;
+            script.friction = friction;
+        }
+	
+	}
+}
--- Assets/code/physics/PhysicsObject.cs
+++ Assets/code/physics/PhysicsObject.cs
@@ -6,6 +6,7 @@ public class PhysicsObject : MonoBehaviour
     Rigidbody rb;
     Vector3 lastpos;
     bool collided = false;
+    private float radius;
     List<Collision> colliderNormals;
     public Vector3 GlobalGravity = new Vector3(0, 0, 0);
     public Vector3 gravity = new Vector3(0, 0, 0);
@@ -15,11 +16,13 @@ public class PhysicsObject : MonoBehaviour
     public float friction = 0;
     public float mass = 1;
     public float airResistance = 0;
-
     public float maxVelocity = 0;
+    private float adj = 1;
 
     void Awake()
     {
+        radius = transform.localScale.x * 0.5f;
+        //Debug.Log(radius);
         colliderNormals = new List<Collision>();
         rb = GetComponent<Rigidbody>();
     }
@@ -55,7 +58,63 @@ public class PhysicsObject : MonoBehaviour
             velocity = velocity.normalized * maxVelocity;
         }
         Debug.DrawRay(transform.position, velocity.normalized,Color.red,4);
-        rb.MovePosition(transform.position += velocity * Time.fixedDeltaTime);
+        if (adj == 1)
+        {
+            adj = precalc(velocity);
+           // Debug.Log("adj:" + velocity * adj);
+            rb.MovePosition(transform.position += velocity * adj * Time.fixedDeltaTime);
+        }
+        else
+        {
+            rb.MovePosition(transform.position += velocity * adj * Time.fixedDeltaTime);
+            //Debug.Log("reverse adj:" + velocity * adj);
+            adj = 1;
+        }
+
+    }
+
+    //used to scale down velocity to avoid ugly clipping
+    // disabled for now since it doesnt work 100%
+    
+    float precalc(Vector3 vel)
+    {
+        float mag = (vel*Time.fixedDeltaTime).magnitude;
+        // maximum allowed clipping limit by the ball
+        float limit = 0.05f;
+        if (mag < limit) {
+            return 1;
+        }
+        Vector3 edge = transform.position + vel.normalized * radius;
+        if (!(Physics.Linecast(edge, edge+vel*Time.fixedDeltaTime)))
+        {
+            return 1;
+        }
+        
+        int iterations = 10;
+        float scale = 0.25f;
+        float search = 0.5f;
+        float curr = 1.0f;
+        for (int i = 0; i < iterations; i++) {
+            if ((Physics.Linecast(edge, edge + search*vel * Time.fixedDeltaTime)))
+            {
+                curr = search;
+                search -= scale;
+                //Debug.Log("search decreased: " + search);
+                if (curr*mag < limit)
+                {
+                  //  Debug.Log("broke out of loop");
+                    break;
+                }
+            }
+            else
+            {
+                search += scale;
+                //Debug.Log("search increased: " + search);
+            }
+            scale *= 0.5f;
+        }
+        //Debug.Log(curr);
+        return curr;
     }
 
     Vector3 CalculateGravity()
@@ -80,7 +139,7 @@ public class PhysicsObject : MonoBehaviour
 
     bool spherecollision(Vector3 direction)
     {
-        return Physics.OverlapSphere(transform.position + direction * Time.deltaTime, 0.5f).Length >= 2;
+        return Physics.OverlapSphere(transform.position + direction * Time.deltaTime, radius).Length >= 2;
     }
 
     void OnCollisionEnter(Collision collision)
@@ -161,4 +220,17 @@ public class PhysicsObject : MonoBehaviour
         }
         return tempvel - force * Time.fixedDeltaTime;
     }
+
+    public void SetGlobalGravity(Vector3 grav) {
+        GlobalGravity = grav;
+    }
+
+    public void AddForce(Vector3 force) {
+        velocity += force;
+    }
+
+    public void SetVelocity(Vector3 vel) {
+        velocity = vel;
+    }
+        
 }
