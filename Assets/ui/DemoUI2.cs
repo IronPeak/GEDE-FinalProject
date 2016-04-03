df0757ecf098f334bf5f50509dc82215087f525f
IronPeak <HrafnOrri1207@gmail.com>
2016-04-03 14:16:03 +0000

    update

--- Assets/ui/DemoUI2.cs
+++ Assets/ui/DemoUI2.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class DemoUI2 : MonoBehaviour
 {
@@ -7,16 +8,39 @@ public class DemoUI2 : MonoBehaviour
     public Slider Earth1Mass;
     public Slider Earth2Mass;
     public Slider Earth3Mass;
+    public Slider Bouncyness;
+    public Slider Friction;
 
     public GravityObject Earth1;
     public GravityObject Earth2;
     public GravityObject Earth3;
+
+    public List<PhysicsObject> Spheres;
+
+    float oldbouncevalue = 1;
+    float oldfrictionvalue = 1;
 
     void Update()
     {
         Earth1.Mass = Earth1Mass.value * 1000 - 500;
         Earth2.Mass = Earth2Mass.value * 1000 - 500;
         Earth3.Mass = Earth3Mass.value * 1000 - 500;
+        if(Bouncyness.value != oldbouncevalue)
+        {
+            oldbouncevalue = Bouncyness.value;
+            foreach(PhysicsObject s in Spheres)
+            {
+                s.bouncyness = Bouncyness.value;
+            }
+        }
+        if(Friction.value != oldfrictionvalue)
+        {
+            oldfrictionvalue = Friction.value;
+            foreach(PhysicsObject s in Spheres)
+            {
+                s.friction = Friction.value;
+            }
+        }
     }
 
 }
