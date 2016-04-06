9423695393f866579ca18faf06729f886fb498dd
IronPeak <HrafnOrri1207@gmail.com>
2016-04-06 08:39:05 +0000

    update

---
+++ Assets/code/ShowMouse.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowMouse : MonoBehaviour {
+
+	void Start () {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+	
+}
